54a2e67bb5a5383796378a2a8cd0a25d050ebf12
rvergesi <rvergesi@uic.es>
2024-12-07 11:45:07 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '¡Hola, Mundo!'
+
+@app.route('/uic')
+def uic_func():
+    return 'UIC'
+
+if __name__ == '__main__':
+    app.run(debug=True)
